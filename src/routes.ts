c1c0a6cf10aa48864fd0568fd00ae52fce2e4ca5
Tainá Isabela <30873852+TainaIsabela@users.noreply.github.com>
2021-04-20 20:14:01 -0300

    Aula 2 - Maximum Speed (NLW05)
    Criando Migrations, Repositórios, Entidades e rotas

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { SettingsControllers } from "./controllers/SettingsControllers";
+const routes = Router();
+const settingsControllers = new SettingsControllers();
+routes.post("/settings", settingsControllers.create);
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 const express = require("express");
+import "./database";
+import { routes } from "./routes";
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send("olá NLW 05")
-})
-app.post("/users", (request,response)=>{
-    return response.json({message: "Usuário salvo com sucesso!"})
-})
+app.use(express.json());
 
+
+app.use(routes);
 app.listen(3333, () => console.log("Rodando na porta 3333"));
